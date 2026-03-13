aeab3330caa33e1e168deb82ae8ffa2d4d216218
Mouli Dhara <19moulidhara@gmail.com>
2026-03-13 23:36:12 +0530

    Refactor code structure for improved readability and maintainability; optimize performance in key modules.

---
+++ MyChat/server.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.static("public"));
+
+const server = http.createServer(app);
+
+// Socket.IO setup
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+    console.log("user connected:", socket.id);
+
+    socket.on("chatMessage", (msg) => {
+        io.emit("chatMessage", msg);
+    });
+
+    socket.on("disconnect", () => {
+        console.log("user disconnected:", socket.id);
+    });
+});
+
+// MongoDB connect
+mongoose.connect("mongodb+srv://mouli_db_user:<db_password>@cluster0.1ub67ev.mongodb.net/chatdb")
+.then(()=>console.log("MongoDB Connected"))
+.catch((err)=>console.log(err));
+
+// Root route
+app.get("/", (req,res)=>{
+    res.send("Server Running Successfully 🚀");
+});
+
+// Start server
+server.listen(5000, ()=>{
+    console.log("Server running on http://localhost:5000");
+});
